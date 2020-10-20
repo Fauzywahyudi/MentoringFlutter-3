ad54d80b0609c425c062519f882dbcc396fc6abc
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-10-20 11:25:03 +0700

    fix Task 1 & 2

--- Week1/task1_register_profil/lib/main.dart
+++ Week1/task1_register_profil/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Task 1',
+      title: 'Task 1 & 2',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.purple,
